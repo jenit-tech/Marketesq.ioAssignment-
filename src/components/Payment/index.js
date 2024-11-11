84890e9996728c0048331fd5d382fc0197057580
Jenit Joseph Jose <147470538+jenit-tech@users.noreply.github.com>
2024-11-11 08:52:14 +0530

    Update index.js

--- src/components/Payment/index.js
+++ src/components/Payment/index.js
@@ -1,68 +1,63 @@
-import './index.css'
-import Header from '../Header'
-import {useLocation} from 'react-router-dom'
-
-import Footer from '../Footer'
-import './index.css'
+import { useLocation } from 'react-router-dom';
+import Header from '../Header';
+import Footer from '../Footer';
+import './index.css';
 
 const Payment = () => {
-  const location = useLocation()
-  const {name, email, number, children, adult, checkOut, checkIn, count} =
-    location.state || {}
-  return (
-    <>
-      <Header />
-      <div className="booking-one-container">
-        <div className="booking-one-content">
-          <div className="booking-one-center">
-            <div className="payment-card-one">
-              <h2>{name}</h2>
-              <p>{email}</p>
-              <p>{number}</p>
-              <p>
-                {' '}
-                {adult} Adults And {children} Children{' '}
-              </p>
-            </div>
-            <div className="payment-card-two">
-              <img
-                className="image-payment"
-                src="https://assets.ccbp.in/frontend/react-js/travel-trip-steps-successfully-completed-img.png"
-                alt=""
-              />
-              <div>
-                <h2>Order Complete</h2>
-                <p>have questions?</p>
-                <p>contact us</p>
-              </div>
-            </div>
-          </div>
+    const location = useLocation();
+    const { name, email, number, children, adult, checkOut, checkIn, count } = location.state || {};
 
-          <div className="card-four">
-            <div className="card-four-a">
-              <p className="card-four-heading">CHECK-IN</p>
-              <p className="checkin">{checkIn}</p>
-            </div>
-            <div className="card-four-b">
-              <p className="card-four-heading">CHECK-OUT</p>
-              <p className="checkout">{checkOut}</p>
-            </div>
-            <div className="card-four-c">
-              <p className="card-four-heading">ROOMS</p>
-              <div className="card-four-d">
-                <p className="card-four-description">{count}</p>
-              </div>
-            </div>
+    return (
+        <>
+            <Header />
+            <div className="booking-one-container">
+                <div className="booking-one-content">
+                    <div className="booking-one-center">
+                        <div className="payment-card-one">
+                            {name && <h2>{name}</h2>}
+                            {email && <p>{email}</p>}
+                            {number && <p>{number}</p>}
+                            {adult && children && <p>{adult} Adults And {children} Children</p>}
+                        </div>
+                        <div className="payment-card-two">
+                            <img
+                                className="image-payment"
+                                src="https://assets.ccbp.in/frontend/react-js/travel-trip-steps-successfully-completed-img.png"
+                                alt="order complete"
+                            />
+                            <div>
+                                <h2>Order Complete</h2>
+                                <p>Have questions?</p>
+                                <p>Contact us</p>
+                            </div>
+                        </div>
+                    </div>
 
-            <div>
-              <button className="booking-button">12430</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
-  )
-}
+                    <div className="card-four">
+                        <div className="card-four-a">
+                            <p className="card-four-heading">CHECK-IN</p>
+                            {checkIn && <p className="checkin">{checkIn}</p>}
+                        </div>
+                        <div className="card-four-b">
+                            <p className="card-four-heading">CHECK-OUT</p>
+                            {checkOut && <p className="checkout">{checkOut}</p>}
+                        </div>
+                        <div className="card-four-c">
+                            <p className="card-four-heading">ROOMS</p>
+                            <div className="card-four-d">
+                                {count && <p className="card-four-description">{count}</p>}
+                            </div>
+                        </div>
 
-export default Payment
+                        <div>
+                            <button className="booking-button">12430</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default Payment;
